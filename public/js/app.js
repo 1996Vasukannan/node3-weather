069a354c5a59e0c6a8c704868a2cea94152630c5
vasukannan <vasukannan2996@gmail.com>
2021-11-11 21:15:41 -0800

    changes made for heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ WeatherForm.addEventListener('submit',(e)=>{
     const location=search.value;
     messageOne.textContent='loading...';
 
-    fetch("http://localhost:3000/weather?address="+location).then((response)=>{
+    fetch("/weather?address="+location).then((response)=>{
     response.json().then((data)=>{
         messageOne.textContent='';
         console.log(data);
